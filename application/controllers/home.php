8db6119369a40e662a184691c87f8187f865ee11
Liang Sun <sunly917@gmail.com>
2015-04-19 13:56:04 +0800

    number of customers in home page.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,14 +7,16 @@ class Home extends CI_Controller {
 		parent::__construct();
 
 		if (!$this->ion_auth->logged_in()) {
-			redirect(base_url('auth/login'),'refresh');
+			redirect(base_url('login'),'refresh');
 		}
 		elseif (!$this->ion_auth->is_admin()) //remove this elseif if you want to enable this for non-admins
 		{
 			//redirect them to the home page because they must be an administrator to view this
 			return show_error('You must be an administrator to view this page.');
 		}
+
 		$this->load->library('ion_auth');
+		$this->load->model(array('customer'));
 	}
 
 	function index()
@@ -22,6 +24,8 @@ class Home extends CI_Controller {
 		$this->data['title'] = 'Home';
 		$this->data['status'] = $this->session->flashdata('status');
 		$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+
+    $this->data['number_of_customers'] = $this->customer->count();
 		
 		$this->load->view('otrack/home', $this->data);
 	}
--- application/views/otrack/home.php
+++ application/views/otrack/home.php
@@ -9,6 +9,14 @@
 
 	<a class="btn btn-default" href="<?php echo base_url('customers/create'); ?>">Add Customer</a>
 	<a class="btn btn-default" href="<?php echo base_url('orders/create'); ?>">Add Order</a>
+
+  <hr>
+  <div class="list-group">
+    <a class="list-group-item" href="<?php echo base_url('customers'); ?>">
+      <span class="badge"><?php echo $number_of_customers; ?></span>
+      <b>Customers</b>
+    </a>
+  </div>
 </div>
 
 <?php $this->load->view('otrack/common/footer'); ?>
