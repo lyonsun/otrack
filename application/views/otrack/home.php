0ada03856b6519f7c76bbfa7d45d4edc9ea91284
Liang Sun <sunly917@gmail.com>
2015-06-15 00:04:13 +0800

    home page fix.

--- application/views/otrack/home.php
+++ application/views/otrack/home.php
@@ -15,22 +15,38 @@
   </div>
 
   <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">Orders <small><a href="<?php echo base_url('orders'); ?>"><i class="fa fa-fw fa-external-link"></i> Details</a></small></h3>
-      </div>
-    <ul class="list-group">
-      <li class="list-group-item"><b>Total Number of Orders:</b> <span class="progress-bar-success badge pull-right"><?php echo $number_of_orders; ?></span></li>
-    </ul>
-      <div class="panel-body">
-        <div class="row">
-          <div class="col-md-6">
-            <div id="chart1"></div>
-          </div>
-          <div class="col-md-6">
-            <div id="chart2"></div>
-          </div>
-        </div> 
-      </div>
+    <div class="list-group">
+      <a class="list-group-item" href="<?php echo base_url('orders'); ?>">
+        <span class="progress-bar-success badge"><?php echo $number_of_orders; ?></span>
+        <b>Orders</b>
+      </a>
+    </div>
+    <table class="table table-bordered text-center">
+      <thead class="bg-warning">
+        <tr>
+          <th class="text-center">International</th>
+          <th class="text-center">Domestic</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><span class="progress-bar-info badge"><?php echo $number_of_international_orders; ?></span></td>
+          <td><span class="progress-bar-info badge"><?php echo $number_of_domestic_orders; ?></span></td>
+        </tr>
+      </tbody>
+      <thead class="bg-warning">
+        <tr>
+          <th class="text-center">Pending</th>
+          <th class="text-center">Finished</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><?php echo anchor(base_url('orders').'/index/1', '<span class="progress-bar-danger badge">'.$number_of_pending_orders.' <i class="fa fa-fw fa-arrow-right"></i></span>'); ?></td>
+          <td><?php echo anchor(base_url('orders').'/index/2', '<span class="badge">'.$number_of_finished_orders.' <i class="fa fa-fw fa-arrow-right"></i></span>'); ?></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
   
   
@@ -40,67 +56,7 @@
 
 <script>
   $(function() {
-    $('#chart1').highcharts({
-        chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          // spacing: [50,50,50,50],
-          plotShadow: false
-        },
-        title: {
-          text: 'International vs Domestic'
-        },
-        credits: false,
-        tooltip: {
-        },
-        plotOptions: {
-          pie: {
-            dataLabels: {
-              enabled: false
-            },
-            showInLegend: true
-          }
-        },
-        series: [{
-          type: 'pie',
-          name: 'Count',
-          data: [
-              ['International',     <?php echo $number_of_international_orders; ?>],
-              ['Domestic',   <?php echo $number_of_domestic_orders; ?>]
-          ]
-        }]
-    });
 
-    $('#chart2').highcharts({
-        chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          // spacing: [50,50,50,50],
-          plotShadow: false
-        },
-        title: {
-          text: 'Pending vs Finished'
-        },
-        credits: false,
-        tooltip: {
-        },
-        plotOptions: {
-          pie: {
-            dataLabels: {
-              enabled: false
-            },
-            showInLegend: true
-          }
-        },
-        series: [{
-          type: 'pie',
-          name: 'Count',
-          data: [
-              ['Pending',     <?php echo $number_of_pending_orders; ?>],
-              ['Finished',   <?php echo $number_of_finished_orders; ?>]
-          ]
-        }]
-    });
   });
 </script>
 
