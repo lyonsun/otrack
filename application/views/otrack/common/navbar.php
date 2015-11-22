90b8890c19eb7603b8591770be7000de83ffc406
Liang Sun <sunly917@gmail.com>
2015-11-22 22:17:42 +0200

    ui fix: slide sidebar in from left rather than right when on mobile devices.

--- application/views/otrack/common/navbar.php
+++ application/views/otrack/common/navbar.php
@@ -1,7 +1,7 @@
 
 
 <nav class="navbar navbar-default navbar-fixed">
-  <div class="container">
+  <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
       <span class="sr-only">Toggle navigation</span>
